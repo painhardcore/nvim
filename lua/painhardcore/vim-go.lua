0dfe8ab957c479415cab211b0fc2d48a498243e7
Andrey Yurchenkov <painhardcore@gmail.com>
2022-08-06 00:53:25 +0300

    add better highlighting

--- lua/painhardcore/vim-go.lua
+++ lua/painhardcore/vim-go.lua
@@ -1,2 +1,8 @@
 vim.g.go_def_mode = "gopls"
 vim.g.go_info_mode = "gopls"
+vim.g.go_auto_type_info = 1
+vim.g.go_auto_sameids = 1
+vim.g.go_highlight_fields = 1
+vim.g.go_highlight_types = 1
+vim.g.go_highlight_function_calls = 1
+vim.g.go_highlight_operators = 1
