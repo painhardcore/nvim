e11429747449dfde0dbce6fbf841f962865a1956
Andrey Yurchenkov <painhardcore@gmail.com>
2022-08-25 02:34:00 +0300

    sync

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -104,6 +104,11 @@ _G.packer_plugins = {
     path = "/Users/kotey/.local/share/nvim/site/pack/packer/start/dashboard-nvim",
     url = "https://github.com/glepnir/dashboard-nvim"
   },
+  ["far.vim"] = {
+    loaded = true,
+    path = "/Users/kotey/.local/share/nvim/site/pack/packer/start/far.vim",
+    url = "https://github.com/brooth/far.vim"
+  },
   ["git-worktree.nvim"] = {
     loaded = true,
     path = "/Users/kotey/.local/share/nvim/site/pack/packer/start/git-worktree.nvim",
